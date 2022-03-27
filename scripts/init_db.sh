10fa594a53fee60561a7f37cca8a358c38450ab4
Anuvrat <anuvrats06@gmail.com>
2022-03-27 07:11:25 +0530

    Minor: Fixes `sudo` privilege for podman in `init_db.sh`

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -26,7 +26,7 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 
 if  [[ -z "${SKIP_PODMAN}" ]]
 then 
-  sudo podman run \
+  podman run \
     -e POSTGRES_USER=${DB_USER} \
     -e POSTGRES_PASSWORD=${DB_PASSWORD} \
     -e POSTGRES_DB=${DB_NAME} \
