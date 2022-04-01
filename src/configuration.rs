dc3024f6ce34806cd9aa7c66ec1a45aa40c96044
Anuvrat <anuvrats06@gmail.com>
2022-04-01 23:06:21 +0530

    Major: General `send_email()` function using SengGrid@v3

--- src/configuration.rs
+++ src/configuration.rs
@@ -4,10 +4,30 @@ use sqlx::{
     postgres::{PgConnectOptions, PgSslMode},
     ConnectOptions,
 };
+
+use crate::domain::SubscriberEmail;
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub application: ApplicaitonSettings,
+    pub email_client: EmailClientSettings,
+}
+#[derive(serde::Deserialize)]
+pub struct EmailClientSettings {
+    pub base_url: String,
+    pub sender_email: String,
+    pub auth_token: Secret<String>,
+    pub timeout_milliseconds: u64,
+}
+
+impl EmailClientSettings {
+    pub fn sender(&self) -> Result<SubscriberEmail, String> {
+        SubscriberEmail::parse(self.sender_email.clone())
+    }
+
+    pub fn timeout(&self) -> std::time::Duration {
+        std::time::Duration::from_millis(self.timeout_milliseconds)
+    }
 }
 
 #[derive(serde::Deserialize)]
